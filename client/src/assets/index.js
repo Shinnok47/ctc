723c934a7164e3c7bf07d7fa574bf073875df842
shinnok47 <shinnok1100@gmail.com>
2023-04-22 19:22:44 +0530

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+// import yariga from './yariga.svg';
+
+// export {
+//   logo,
+//   yariga,
+// };
+
+
+// import ctc from './ctc.svg'
+
+// export
+// {
+//   ctc,
+// }
+
+
+import ctc from '../assets/logo.png'
+
+export
+{
+  ctc,
+}
